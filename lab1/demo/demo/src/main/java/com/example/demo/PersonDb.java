b6c1cb26663e64bd3b78e64dbe110271873789dc
DorotaMatkowska <dorotamatkowska23@gmail.com>
2021-03-22 23:46:12 +0100

    Zadanie1

---
+++ lab1/demo/demo/src/main/java/com/example/demo/PersonDb.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PersonDb {
+    public Person[] persons = new Person[1000];
+
+    public void Add(Person person){
+        persons[0] = person;
+    }
+
+    public Person[] getPersonArray() {
+        return persons;
+    }
+}
