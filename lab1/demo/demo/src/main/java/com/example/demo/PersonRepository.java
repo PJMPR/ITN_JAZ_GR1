ad7c3f0693cf35f5783dff46eae119ca66e67f77
r4v88 <sochacki.rfl@gmail.com>
2021-03-24 21:46:33 +0100

    Person Repository completed

--- lab1/demo/demo/src/main/java/com/example/demo/PersonRepository.java
+++ lab1/demo/demo/src/main/java/com/example/demo/PersonRepository.java
@@ -1,4 +1,25 @@
 package com.example.demo;
 
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
 public class PersonRepository {
+
+    private Persondb persondb;
+
+
+    public PersonRepository(Persondb persondb) {
+        this.persondb = persondb;
+    }
+
+    public void save(Person person){
+        persondb.addToPersonArray(person);
+    }
+
+    public Persondb getDb(){
+        return persondb;
+    }
+
 }
