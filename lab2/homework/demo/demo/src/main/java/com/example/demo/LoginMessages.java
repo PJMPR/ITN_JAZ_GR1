56689a8fd4b82f24ed765397bf3f563dd15d59fd
mbiedrzycki <mbiedrzycki@gwo.pl>
2021-04-12 21:55:09 +0200

    green test

---
+++ lab2/homework/demo/demo/src/main/java/com/example/demo/LoginMessages.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LoginMessages {
+
+    private String username;
+
+    public String welcome() {return "Witaj";}
+
+    public String getUsernameMessage() {return "podaj nazwe uzytkownika:";}
+
+    public void setUsername(String username) {this.username = username;}
+
+    public String getLoggedAsMessage() {return "Zalogowany jako "+ this.username;}
+}
