cb4cce981e062f928a9bb101b3fa094d3b0bcde3
Adrian Matyszczak <48859003+Adrian19850@users.noreply.github.com>
2021-04-13 00:09:30 +0200

    Add files via upload

--- lab2/homework/demo/demo/src/main/java/com/example/demo/LoginMessages.java
+++ lab2/homework/demo/demo/src/main/java/com/example/demo/LoginMessages.java
@@ -2,30 +2,26 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 @Configuration
 public class LoginMessages {
 
-    String user;
+    String username;
 
-    public LoginMessages loginMessages(){
-        return  new LoginMessages();
-    }
     @Value("${message.welcome}") String welcome;
     public String welcome() {
         return welcome;
     }
-@Value("${message.provideUsername}") String provideUsername;
+    @Value("${message.provideUsername}") String provideUsername;
     public String getUsernameMessage() {
         return provideUsername;
     }
 
     public void setUsername(String username) {
-        user = username;
+        this.username = username;
     }
-@Value("${message.loggedAsMessage}") String loggedAsMessage;
+    @Value("${message.loggedAsMessage}") String loggedAsMessage;
     public String getLoggedAsMessage() {
-        return loggedAsMessage + " " + user;
+        return loggedAsMessage + " " + username;
     }
 }
