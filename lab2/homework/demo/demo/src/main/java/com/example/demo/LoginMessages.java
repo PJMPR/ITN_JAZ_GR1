32619abaeb16e9ed41024876d180d109fa68ec10
Kaczmarek-M <52320814+Kaczmarek-M@users.noreply.github.com>
2021-04-12 23:45:30 +0200

    firstCommit

--- lab2/homework/demo/demo/src/main/java/com/example/demo/LoginMessages.java
+++ lab2/homework/demo/demo/src/main/java/com/example/demo/LoginMessages.java
@@ -1,1 +1,22 @@
+package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LoginMessages {
+
+    String username;
+
+    @Value("${message.welcome}") String welcome;
+    public String welcome() {return welcome;}
+
+    @Value("${message.provideUsername}") String provideUsername;
+    public String getUsernameMessage() {return provideUsername;}
+
+    public void setUsername(String username) {this.username = username;}
+
+    @Value("${message.username}") String user;
+    public String getLoggedAsMessage() {return user + " " + username;}
+}
+© 2021 GitHub, Inc.
