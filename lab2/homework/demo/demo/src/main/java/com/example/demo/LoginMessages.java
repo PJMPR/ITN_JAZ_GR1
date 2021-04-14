a66ab8ad59366aed5f0986f5eca0b789054dd912
Adrian Matyszczak <48859003+Adrian19850@users.noreply.github.com>
2021-04-14 10:17:24 +0200

    Update LoginMessages.java

--- lab2/homework/demo/demo/src/main/java/com/example/demo/LoginMessages.java
+++ lab2/homework/demo/demo/src/main/java/com/example/demo/LoginMessages.java
@@ -6,22 +6,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class LoginMessages {
 
-   String username;
+    String username;
 
     @Value("${message.welcome}") String welcome;
-    public String welcome() {
-        return welcome;
-    }
-    @Value("${message.provideUsername}") String provideUsername;
-    public String getUsernameMessage() {
-        return provideUsername;
-    }
+    public String welcome() {return welcome;}
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    @Value("${message.provideUsername}") String provideUsername;
+    public String getUsernameMessage() {return provideUsername;}
+
+    public void setUsername(String username) {this.username = username;}
+
     @Value("${message.username}") String user;
-    public String getLoggedAsMessage() {
-        return user + " " + username;
-    }
+    public String getLoggedAsMessage() {return user + " " + username;}
 }
