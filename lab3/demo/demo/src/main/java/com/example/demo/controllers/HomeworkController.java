006bdc47ccf320b3284613961bce7a2e13a550a6
mcicho <48605241+mcicho@users.noreply.github.com>
2021-04-26 18:02:17 +0200

    lab3 homework

---
+++ lab3/demo/demo/src/main/java/com/example/demo/controllers/HomeworkController.java
@@ -0,0 +1,39 @@
+package com.example.demo.controllers;
+
+import com.example.demo.contract.Person;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("homework") // localhost:8080/homework
+public class HomeworkController {
+
+//    @GetMapping("test/{some_value}")
+//    public ResponseEntity pathParams(@PathVariable("some_value") String pathParam, @RequestParam("name") String name){
+//        return ResponseEntity.ok(pathParam+name);
+//    }
+
+    @GetMapping("{path}")
+    public ResponseEntity pathParamsQuery(@PathVariable("path") String pathParam, @RequestParam("query") String queryParam){
+        return ResponseEntity.ok("path:" + pathParam + " query:" + queryParam);
+    }
+
+    @PostMapping("person") // localhost:8080/homework/person
+    public ResponseEntity<Person> person(
+            @RequestBody Person person){
+        return ResponseEntity.ok(person);
+    }
+
+    @PutMapping("person/{path}") // localhost:8080/homework/person/1
+    public ResponseEntity<Person> person(
+            @PathVariable("path") String path,
+            @RequestBody Person person){
+        return ResponseEntity.ok(person);
+    }
+
+    @DeleteMapping("person") // localhost:8080/homework/person/1
+    public ResponseEntity<String> person(
+            @PathVariable("path") String path){
+        return ResponseEntity.ok("");
+    }
+}
