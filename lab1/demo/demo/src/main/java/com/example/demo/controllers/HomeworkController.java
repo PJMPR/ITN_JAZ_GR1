2a919164195a573dabfd67d829740f99508b891f
Rafal <sochacki.rfl@gmail.com>
2021-04-26 22:04:02 +0200

    Homework Controller

---
+++ lab1/demo/demo/src/main/java/com/example/demo/controllers/HomeworkController.java
@@ -0,0 +1,35 @@
+package com.example.demo.controllers;
+
+import com.example.demo.contract.Person;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("homework")
+public class HomeworkController {
+    @GetMapping()
+    public ResponseEntity x() {
+        return ResponseEntity.ok("x");
+    }
+
+    @GetMapping("{location}")
+    public ResponseEntity params(@PathVariable("location") String location,
+                                 @RequestParam("query") String query) {
+        return ResponseEntity.ok("path:" + location + " query:" + query);
+    }
+
+    @PostMapping("person")
+    public ResponseEntity postPerson(@RequestBody Person person) {
+        return ResponseEntity.ok(person);
+    }
+
+    @PutMapping("person/{id}")
+    public ResponseEntity updatePerson(@PathVariable("id") String id, @RequestBody Person person) {
+        return ResponseEntity.ok(person);
+    }
+
+    @DeleteMapping("person/{id}")
+    public ResponseEntity deletePerson(@PathVariable("id") String id) {
+        return ResponseEntity.ok("");
+    }
+}
