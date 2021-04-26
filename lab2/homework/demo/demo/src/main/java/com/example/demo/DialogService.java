e7307256cf9d6358a1472223ef895131d5dab86e
s16216 <s16216@pjwstk.edu.pl>
2021-04-26 21:05:16 +0200

    lab3

--- lab2/homework/demo/demo/src/main/java/com/example/demo/DialogService.java
+++ lab2/homework/demo/demo/src/main/java/com/example/demo/DialogService.java
@@ -1,21 +1,17 @@
 package com.example.demo;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DialogService {
 
-    private LoginMessages message;
+    LoginMessages messages;
 
-    public DialogService (LoginMessages message){
-
-        this.message = message;
+    public DialogService(LoginMessages messages) {
+        this.messages = messages;
     }
 
     public LoginMessages getLoginMessages() {
-
-        return message;
+        return messages;
     }
 }
---
+++ lab3/demo/demo/src/main/java/com/example/demo/controllers/HelloController.java
@@ -0,0 +1,55 @@
+package com.example.demo.controllers;
+
+import com.example.demo.contract.Car;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+
+@RestController
+@RequestMapping("hello") // localhost:8080/hello
+public class HelloController {
+
+    @GetMapping("test") // localhost:8080/hello/test z metoda HttpGet
+    public ResponseEntity<String> test() {
+        ResponseEntity<String> response = new ResponseEntity<String>("Hello World", HttpStatus.OK);
+        return response;
+    }
+
+    @GetMapping("model")// localhost:8080/hello/model z metoda HttpGet
+    public ResponseEntity<Car> getNewCarModel() {
+
+//        ResponseEntity<Car> response = new ResponseEntity<Car>(new Car(
+//                "Audi",
+//                "GD0001",
+//                100000,
+//                false,
+//                30000.0), HttpStatus.OK);
+
+        ResponseEntity response = ResponseEntity.ok(new Car(
+                "Audi",
+                "GD0001",
+                100000,
+                false,
+                30000.0)
+                );
+        return response;
+    }
+
+    @PostMapping("model")// localhost:8080/hello/model z metoda HttpPost
+    public ResponseEntity<Car> saveCar(
+            @RequestBody Car car){
+        return ResponseEntity.ok(car);
+    }
+
+    @GetMapping("test/{some_value}")
+    public ResponseEntity pathParams(@PathVariable("some_value") String pathParam, @RequestParam("name") String name){
+        return ResponseEntity.ok(pathParam+name);
+    }
+
+    @GetMapping("test/query")
+    public ResponseEntity queryParams(@RequestParam("name") String name){
+        return ResponseEntity.ok(name);
+    }
+}
