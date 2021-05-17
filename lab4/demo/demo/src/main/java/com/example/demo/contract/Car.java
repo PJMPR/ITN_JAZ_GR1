632ed53b7f5c8378141e638b4358c98d6eeab2b6
s18117 <s18117@pjwstk.edu.pl>
2021-05-17 23:36:04 +0200

    Zadanie 4

--- lab4/demo/demo/src/main/java/com/example/demo/contract/Car.java
+++ lab4/demo/demo/src/main/java/com/example/demo/contract/Car.java
@@ -8,6 +8,16 @@ public class Car {
     private boolean hasAccidents;
     private double price;
 
+    int ID;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
     public Car(String model, String registrationNumber, int milleage, boolean hasAccidents, double price) {
         this.model = model;
         this.registrationNumber = registrationNumber;
