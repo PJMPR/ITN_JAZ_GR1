a59b7ff61f1e93a65108e5759aa530cdbc96a2fb
Adrian Matyszczak <48859003+Adrian19850@users.noreply.github.com>
2021-05-17 22:38:54 +0200

    Add files via upload

--- lab4/demo/demo/src/main/java/com/example/demo/contract/Car.java
+++ lab4/demo/demo/src/main/java/com/example/demo/contract/Car.java
@@ -1,58 +1,16 @@
-package com.example.demo.contract;
+private boolean hasAccidents;
+private double price;
 
-public class Car {
+        int ID;
 
-    private String model;
-    private String registrationNumber;
-    private int milleage;
-    private boolean hasAccidents;
-    private double price;
+public int getID() {
+        return ID;
+        }
 
-    public Car(String model, String registrationNumber, int milleage, boolean hasAccidents, double price) {
+public void setID(int ID) {
+        this.ID = ID;
+        }
+
+public Car(String model, String registrationNumber, int milleage, boolean hasAccidents, double price) {
         this.model = model;
         this.registrationNumber = registrationNumber;
-        this.milleage = milleage;
-        this.hasAccidents = hasAccidents;
-        this.price = price;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public String getRegistrationNumber() {
-        return registrationNumber;
-    }
-
-    public void setRegistrationNumber(String registrationNumber) {
-        this.registrationNumber = registrationNumber;
-    }
-
-    public int getMilleage() {
-        return milleage;
-    }
-
-    public void setMilleage(int milleage) {
-        this.milleage = milleage;
-    }
-
-    public boolean isHasAccidents() {
-        return hasAccidents;
-    }
-
-    public void setHasAccidents(boolean hasAccidents) {
-        this.hasAccidents = hasAccidents;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-}
