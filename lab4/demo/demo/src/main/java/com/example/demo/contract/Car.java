417bf116cb67ca6c85d2387f76380967835309b8
vechten <hk190696@gmail.com>
2021-05-18 21:44:28 +0200

    zmiana nazwy gettera i settera xddd autmatycznie generowany kod robi druga z malej

--- lab4/demo/demo/src/main/java/com/example/demo/contract/Car.java
+++ lab4/demo/demo/src/main/java/com/example/demo/contract/Car.java
@@ -17,11 +17,11 @@ public class Car {
         this.price = price;
     }
 
-    public int getId() {
+    public int getID() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setID(int id) {
         this.id = id;
     }
 
