b8cc46685b647d3e336438f11aa3a89b7eda6f45
Adam Urbanowicz <aurbanow@pjwstk.edu.pl>
2021-05-29 11:13:09 +0200

    fix unit tests

--- lab5/demo/demo/src/test/java/com/example/demo/CarsApiTests.java
+++ lab5/demo/demo/src/test/java/com/example/demo/CarsApiTests.java
@@ -9,14 +9,17 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.BasicJsonTester;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
+@SpringBootTest()
 @AutoConfigureMockMvc
+@TestPropertySource(
+        locations = "classpath:application-integrationtests.properties")
 public class CarsApiTests {
 
     @Autowired
