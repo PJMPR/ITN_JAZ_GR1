b86eabbe2551028d13f597ddd0d6c916d0659032
KamilKornatowski <61655870+KamilKornatowski@users.noreply.github.com>
2021-05-31 18:59:14 +0200

    Add files via upload

--- lab5/demo/demo/src/main/java/com/example/demo/repositories/CarRepository.java
+++ lab5/demo/demo/src/main/java/com/example/demo/repositories/CarRepository.java
@@ -1,11 +1,11 @@
 package com.example.demo.repositories;
 
+import com.example.demo.contract.Car;
 import com.example.demo.contract.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface PersonRepository extends JpaRepository<Person, Integer> {
-
-    public List<Person> getAllByName(String name);
+public interface CarRepository extends JpaRepository<Car, Integer> {
+    public List<Car> getAllByModel(String name);
 }
