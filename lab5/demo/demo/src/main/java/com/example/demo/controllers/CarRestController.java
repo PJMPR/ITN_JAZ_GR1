6c2d5908e56e9cacbee0ef50f77e2da9c5627410
mbiedrzycki <mbiedrzycki@gwo.pl>
2021-05-31 23:44:06 +0200

    lab 5

---
+++ lab5/demo/demo/src/main/java/com/example/demo/controllers/CarRestController.java
@@ -0,0 +1,63 @@
+package com.example.demo.controllers;
+
+import com.example.demo.contract.Car;
+import com.example.demo.services.CarDataService;
+import com.example.demo.services.CarService;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("car") // localhost:8080/car
+public class CarRestController {
+
+
+    final private CarDataService dataService;
+
+
+    public CarRestController( CarDataService dataService)
+    {
+        this.dataService =dataService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<Car>> getAll(@RequestParam("name") @DefaultValue("") String name){
+
+        if(name==null || name == "")
+            return ResponseEntity.ok(dataService.getAll());
+        return ResponseEntity.ok(dataService.getByName(name));
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Car> getById(@PathVariable("id") int id){
+        Car result = dataService.getById(id);
+        if(result==null)
+            return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping()
+    public ResponseEntity<Car> saveCar(@RequestBody Car car){
+
+        dataService.save(car);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Car> updateCar(@PathVariable("id") int id, @RequestBody Car car){
+
+        Car result = dataService.update(id, car);
+        if(result == null) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity deleteCar(@PathVariable("id") int id){
+        Car deletedCar = dataService.delete(id);
+        if(deletedCar==null) return ResponseEntity.notFound().build();
+        return ResponseEntity.noContent().build();
+    }
+
+}
