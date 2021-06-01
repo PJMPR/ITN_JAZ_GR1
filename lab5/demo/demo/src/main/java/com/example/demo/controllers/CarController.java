d3218848089797b99702fdf101c5567339d9e4f5
Kaczmarek-M <52320814+Kaczmarek-M@users.noreply.github.com>
2021-06-01 21:22:39 +0200

    first commit

---
+++ lab5/demo/demo/src/main/java/com/example/demo/controllers/CarController.java
@@ -0,0 +1,49 @@
+package com.example.demo.controllers;
+
+import com.example.demo.contract.Car;
+import com.example.demo.contract.Person;
+import com.example.demo.services.CarService;
+import com.fasterxml.jackson.annotation.JsonTypeId;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("cars") //localhost:8080/cars
+public class CarController {
+
+    final CarService serviceCar;
+    public CarController(CarService serviceCar){
+        this.serviceCar = serviceCar;
+    }
+    @GetMapping()
+    public ResponseEntity<List<Car>> getAllCar(){
+        return ResponseEntity.ok(serviceCar.getAllCar());
+    }
+    @GetMapping("{id}")
+    public ResponseEntity<Car> getByIdCar(@PathVariable("id") int id){
+        Car result = serviceCar.getByIdCar(id);
+        if(result==null)
+            return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(result);
+    }
+    @PostMapping()
+    public ResponseEntity<Car> saveCar(@RequestBody Car car){
+        serviceCar.saveCar(car);
+        return ResponseEntity.noContent().build();
+    }
+    @PutMapping("{id}")
+    public ResponseEntity<Car> updateCar(@PathVariable("id") int id, @RequestBody Car car){
+
+        Car result = serviceCar.Update(id, car);
+        if(result == null) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok().build();
+    }
+    @DeleteMapping("{id}")
+    public ResponseEntity deleteCar(@PathVariable("id") int id){
+        Car deletedCar = serviceCar.delete(id);
+        if(deletedCar==null) return ResponseEntity.notFound().build();
+        return ResponseEntity.noContent().build();
+    }
+}
