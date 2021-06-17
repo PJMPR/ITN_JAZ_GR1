a246b309d19a95ec4ee987f185ee23ac3318fbad
Adrian Matyszczak <48859003+Adrian19850@users.noreply.github.com>
2021-06-17 07:00:03 +0200

    vn

--- lab6/demo/demo/src/main/java/com/example/demo/repositories/CarRepository.java
+++ lab6/demo/demo/src/main/java/com/example/demo/repositories/CarRepository.java
@@ -1,10 +1,11 @@
 package com.example.demo.repositories;
 
-import com.example.demo.contract.Car;
+import com.example.demo.model.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CarRepository extends JpaRepository<Car, Integer> {
-    public List<Car> getAllByModel(String name);
+    public List<Car> findByModel(String name);
+
 }
